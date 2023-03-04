7effb29b08ef296c96378d4a7b3f2be25cf5c1f4
felipeam10 <felipeam10@hotmail.com>
2023-03-04 01:22:41 -0300

    aula04: pacotes e aula05: pacotes externos

---
+++ 1- Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+//toda função com letra maiúscula é exportada e possui um comentário por convenção da linguagem go
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
+}
