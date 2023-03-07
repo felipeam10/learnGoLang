06d0f265a6f381a12486af56e4785d2e6ec4dc98
felipeam10 <felipeam10@hotmail.com>
2023-03-07 15:17:27 -0300

    aula06: variables

---
+++ 2-Variaveis/variaveis.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+	var variavel1 string = "Variavel 1"
+	variavel2 := "Variavel 2"
+
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+
+	var (
+		variavel3 string = "lalala"
+		variavel4 string = "lelele"
+	)
+	fmt.Println(variavel3, variavel4)
+
+	variavel5, variavel6 := "Variavel5", "Variavel 6"
+	fmt.Println(variavel5, variavel6)
+
+	const constante1 string = "Constante 1"
+	fmt.Println(constante1)
+
+	//invertendo valores das variaveis
+	variavel5, variavel6 = variavel6, variavel5
+	fmt.Println(variavel5, variavel6)
+
+}
